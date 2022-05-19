87173d7a61d98f3dbeb2d16bf612e9a05d5095b3
suraj <soorajkrpandit@gmail.com>
2022-05-19 10:51:11 +0530

    Add project files.

---
+++ Day20_MoodAnalyzer/HappySad.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_MoodAnalyzer
+{
+    public class HappySad
+    {
+        string Message;
+       
+        public HappySad(string message)
+        {
+            this.Message = message;
+        }
+
+        public string AnalysingMood()
+        {
+            if (Message.ToUpper().Contains("HAPPY"))
+            {
+                return "HAPPY";
+            }
+            if (Message.ToUpper().Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
---
+++ Day20_MoodAnalyzer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_MoodAnalyzer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mood analyser");
+            HappySad happySad = new HappySad("iam in happy a mood");
+            Console.WriteLine(happySad.AnalysingMood());
+            Console.ReadKey();
+        }
+    }
+}
